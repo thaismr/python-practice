325ad1c66fd224740df2752509d10c24fe5be829
thaismr <thaismr@users.noreply.github.com>
2020-11-25 01:27:12 -0300

    test commit 2

--- procedural/groupby.py
+++ procedural/groupby.py
@@ -1,24 +1,34 @@
 from itertools import groupby
 
 # Given a list (students) of dictionaries (name, grade):
-class_grades = [
+sort_by_grades = [
     {'name': name, 'grade': grade} for name, grade in zip(
         ['Jane', 'John', 'Mary', 'Gina', 'Peter'], ['A', 'A', 'C', 'B-', 'C']
     )
 ]
 
 # Print our list:
-for student in class_grades:
+for student in sort_by_grades:
     print(student)
 
 
 def get_grades(students): return students['grade']  # get all grades
 
 
-class_grades.sort(key=get_grades)
-group_by_grades = groupby(class_grades, get_grades)
+# Sort class data by grades:
+sort_by_grades.sort(key=get_grades)
 
-print(list(group_by_grades))
+print('sort_by_grades:')
+for student in sort_by_grades:
+    print(student)
 
-# Get students grade:
-# grade = lambda
+# Group class data by grades:
+print('#' * 40)
+
+group_by_grades = groupby(sort_by_grades, get_grades)
+
+for grade, students in group_by_grades:
+    print(f'{grade} students:')
+    for student in students:
+        print(student)
+
