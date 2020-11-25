7218baba61f509746216e8b0a69c07eaf71931fe
thaismr <thaismr@users.noreply.github.com>
2020-11-25 13:06:46 -0300

    Going over 'groupby' and making minor naming corrections.

--- procedural/groupby.py
+++ procedural/groupby.py
@@ -1,31 +1,31 @@
 from itertools import groupby
 
 # Given a list (students) of dictionaries (name, grade):
-sort_by_grades = [
+students = [
     {'name': name, 'grade': grade} for name, grade in zip(
         ['Jane', 'John', 'Mary', 'Gina', 'Peter'], ['A', 'A', 'C', 'B-', 'C']
     )
 ]
 
 # Print our list:
-for student in sort_by_grades:
+for student in students:
     print(student)
 
 
-def get_grades(students): return students['grade']  # get all grades
+def get_grades(each_student): return each_student['grade']  # get grade for each student
 
 
 # Sort class data by grades:
-sort_by_grades.sort(key=get_grades)
+students.sort(key=get_grades)  # ATTENTION! Must sort students for 'groupby' to work!
 
-print('sort_by_grades:')
-for student in sort_by_grades:
+print('Students sorted by grade:')
+for student in students:
     print(student)
 
 # Group class data by grades:
 print('#' * 40)
 
-group_by_grades = groupby(sort_by_grades, get_grades)
+group_by_grades = groupby(students, get_grades)
 
 for grade, students in group_by_grades:
     print(f'{grade} students:')
